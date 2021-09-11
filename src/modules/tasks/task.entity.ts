c7679dd9f39ccaffc2ac2178744e9e10964054a6
Hieu <duchieu307@gmail.com>
2021-09-11 21:43:20 +0700

    test husky

--- src/modules/tasks/task.entity.ts
+++ src/modules/tasks/task.entity.ts
@@ -23,8 +23,6 @@ export class Task extends BaseEntity {
   @Column()
   status: TaskStatus;
 
-  @ManyToOne((type) => 
-  
-  User, (user) => user.tasks, { eager: false })
+  @ManyToOne((type) => User, (user) => user.tasks, { eager: false })
   user: User;
 }
